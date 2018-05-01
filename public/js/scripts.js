de70d0061fe9da61b6e267e71928f3c0819b2c3a
Jared Eklin <jaredeklin@gmail.com>
2018-05-01 15:28:31 -0600

    Add dynamic drop down menu, add GET and POST fetch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,15 @@
+// const locals = require('../../server.js')
 const currentColors = [];
 const lockedColors = [];
+let projects = [];
 
 $('.new-palette-btn').on('click', colorsArray);
 $('.color-section').on('click', '.lock-btn', toggleLock);
+$('.save-project-btn').on('click', saveProject);
+// $('.save-project-btn').on('click', getProjects);
+
+$('.save-palette-btn').on('click', savePalette);
+$('#drop-down-menu').on('click', changeProject)
   
 colorsArray();
 
@@ -56,8 +63,80 @@ function toggleLock() {
   } else {
     lockedColors.push(color);
   }
-  // return lockedColors.includes(color) ? lockedColors.splice(lockedColors.indexOf(color), 1) : lockedColors.push(color)
 }
+
+async function saveProject(event) {
+  event.preventDefault();
+  const newProject = {
+    projectName: $('.project-name-input').val()
+  }
+
+  const response = await fetch('/api/v1/projects', {
+    method: 'POST',
+    body: JSON.stringify(newProject),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  const projectData = await response.json();
+
+  projects.length = 0;
+  projects = [...projectData]
+  populateDropdown();
+}
+
+async function getProjects(event) {
+  event.preventDefault()
+  const response = await fetch('/api/v1/projects');
+  const projectData = await response.json();
+  
+  projects.push(projectData);
+  populateDropdown();
+}
+
+
+async function savePalette(event) {
+  event.preventDefault();
+
+  const palette = {
+    name: $('.palette-name-input').val(),
+    colors: currentColors
+  }
+
+  const response = await fetch('/api/v1/palettes', {
+    method: 'POST',
+    body: JSON.stringify(palette),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  const data = await response.json();
+  console.log(data)
+  // return data;
+}
+
+
+function changeProject() {
+  console.log('yolo')
+}
+
+function populateDropdown() {
+  $('#drop-down-menu').empty();
+
+  projects.forEach((project, index) => {
+    console.log(project)
+    $('#drop-down-menu').append(`
+      <option>${project.projectName}</option>
+    `)  
+  })
+}
+
+
+
+
+
+
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
+
+app.locals.projects = [];
+app.locals.palettes = [];
 app.locals.title = 'Palette Picker';
 
-app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
-// app.get('/', (request, response) => {
-//   response.send('oh snap! we servering!!!!!');
-// });
+app.get('/api/v1/palettes', (request, response) => {
+  
+  response.status(200).json(app.locals.palettes);
+});
+
+app.get('/api/v1/projects', (request, response) => {
+  response.status(200).json(app.locals.projects);
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+
+  app.locals.palettes.push(request.body);
+  response.status(201).json(app.locals.palettes);
+});
+
+app.post('/api/v1/projects', (request, response) => {
+
+  app.locals.projects.push(request.body);
+  response.status(200).json(app.locals.projects);
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} running on localhost:${app.get('port')}.`);
 });
+
