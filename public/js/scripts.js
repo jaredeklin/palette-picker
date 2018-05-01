d1aedd0dbc02e54727b010d7c7b0adacc3e87bf2
Jared Eklin <jaredeklin@gmail.com>
2018-05-01 17:08:01 -0600

    Add select project text to drop down and add display projects

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,13 +2,14 @@
 const currentColors = [];
 const lockedColors = [];
 let projects = [];
+let currentProject;
 
 $('.new-palette-btn').on('click', colorsArray);
 $('.color-section').on('click', '.lock-btn', toggleLock);
 $('.save-project-btn').on('click', saveProject);
 // $('.save-project-btn').on('click', getProjects);
 $('.save-palette-btn').on('click', savePalette);
-$('#drop-down-menu').on('click', changeProject)
+$('#drop-down-menu').on('change', changeProject)
   
 colorsArray();
 
@@ -39,7 +40,7 @@ function appendColors() {
     let lockedClass = "";
 
     if(lockedColors.includes(color)) {
-      lockedClass = "lock"
+      lockedClass = "lock";
     }
 
     $('.color-section').append(`
@@ -67,7 +68,8 @@ function toggleLock() {
 async function saveProject(event) {
   event.preventDefault();
   const newProject = {
-    projectName: $('.project-name-input').val()
+    projectName: $('.project-name-input').val(),
+    projectId: Date.now()
   }
 
   const response = await fetch('/api/v1/projects', {
@@ -79,56 +81,78 @@ async function saveProject(event) {
   const projectData = await response.json();
 
   projects.length = 0;
-  projects = [...projectData]
+  projects = [...projectData];
   populateDropdown();
+  displayProjects();
   $('.project-name-input').val('');
 }
 
-async function getProjects(event) {
-  event.preventDefault()
-  const response = await fetch('/api/v1/projects');
-  const projectData = await response.json();
+// async function getProjects(event) {
+//   event.preventDefault()
+//   const response = await fetch('/api/v1/projects');
+//   const projectData = await response.json();
   
-  projects.push(projectData);
-  populateDropdown();
-}
+//   projects.push(projectData);
+//   populateDropdown();
+// }
 
 
 async function savePalette(event) {
   event.preventDefault();
+  if(currentProject) {
+    const palette = {
+      name: $('.palette-name-input').val(),
+      colors: currentColors,
+      projectId: currentProject.projectId
+    }
 
-  const palette = {
-    name: $('.palette-name-input').val(),
-    colors: currentColors
+    console.log(palette)
+
+    const response = await fetch('/api/v1/palettes', {
+      method: 'POST',
+      body: JSON.stringify(palette),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    const data = await response.json();
+    $('.palette-name-input').val('');
+  } else {
+    alert('Please select a project')
   }
-
-
-  const response = await fetch('/api/v1/palettes', {
-    method: 'POST',
-    body: JSON.stringify(palette),
-    headers: { 'Content-Type': 'application/json' }
-  });
-
-  const data = await response.json();
-  $('.palette-name-input').val('');
 }
 
 
 function changeProject() {
-  console.log('yolo')
+  currentProject = projects.find(project => project.projectName === $(this).val())
+  console.log(currentProject)
 }
 
 function populateDropdown() {
-  $('#drop-down-menu').empty();
+  $('.select-project').nextAll().remove()
 
-  projects.forEach((project, index) => {
-    console.log(project)
+  projects.forEach(project => {
     $('#drop-down-menu').append(`
       <option>${project.projectName}</option>
-    `)  
+    `);
+  });
+}
+
+function displayProjects() {
+  $('.display-projects').empty();
+
+  projects.forEach(project => {
+    console.log(project)
+    $('.display-projects').append(`
+      <ul>${project.projectName}
+        <li></li>
+      </ul>
+    `)
   })
 }
 
+function displayPalette() {
+  
+}
 
 
 
